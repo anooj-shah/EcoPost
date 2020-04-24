56eaa10a9f02ad13246f0660874df2f61e3627ac
YonathanZetune <yonathan@zetune.com>
2020-04-24 15:48:51 -0500

    added flutter init repo

---
+++ mobile/EcoPost/ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Flutter/Flutter.framework/Headers/Flutter.h
---
+++ mobile/EcoPost/ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/YoniZetune/Develop/earthxhack/EcoPost/mobile/EcoPost/ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
